ac140c3b6b8c5e954cf15b75c18e188b404e3432
avsh <avsh@users.noreply.github.com>
2020-07-07 23:43:56 +0300

    update module settings

--- begateway/lib/config/settings.php
+++ begateway/lib/config/settings.php
@@ -23,27 +23,12 @@ return array(
             waPayment::OPERATION_AUTH_ONLY => 'Authorization'
         ),
    ),
-  'DOMAIN_GATEWAY'     => array(
-         'value'        => '',
-         'title'        => /*_wp*/('Payment gateway domain'),
-         'description'  => /*_wp*/('Ask your payment processor for these options values'),
-         'control_type' => waHtmlControl::INPUT,
-   ),
   'DOMAIN_PAYMENTPAGE' => array(
          'value'        => '',
          'title'        => /*_wp*/('Payment page domain'),
          'description'  => /*_wp*/('Ask your payment processor for these options values'),
          'control_type' => waHtmlControl::INPUT,
-   ),
-	'PAYMENT_PAGE_TYPE'  => array(
-        'value'        => '',
-        'title'        => /*_wp*/('Payment page type'),
-        'description'  => '',
-        'control_type' => waHtmlControl::SELECT,
-        'options'      => array(
-            waPayment::OPERATION_HOSTED_PAYMENT_PRIOR_ORDER => /*_wp*/('Redirect'),
-            waPayment::OPERATION_INTERNAL_PAYMENT => /*_wp*/('iFrame')
-        ),
+   )
   ),
   'ENABLE_BANKCARD' => array(
     'value'        => true,
@@ -64,12 +49,6 @@ return array(
          'value'        => '99999999',
          'title'        => /*_wp*/('ERIP service code'),
          'description'  => /*_wp*/('Ask your payment processor for these options values'),
-         'control_type' => waHtmlControl::INPUT,
-   ),
-  'CSS' => array(
-         'value'        => '',
-         'title'        => /*_wp*/('iFrame CSS'),
-         'description'  => '',
          'control_type' => waHtmlControl::INPUT,
    )
 );
